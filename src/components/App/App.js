a04c35357cf95f523aef8ef05c0299407dd000b1
a.rebrov <a.rebrov@cian.ru>
2022-10-02 22:25:46 +0300

    feat: создает страницу 404

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import Header from '../Header/Header';
 import Main from '../Main/Main';
-import Footer from '../Footer/Footer';
+import NotFoundPage from '../NotFoundPage/NotFoundPage';
 import './App.css';
 
 function App() {
   return (
     <div className='page'>
-      <Header />
       <Routes>
         <Route exact path='/' element={<Main />} />
+        <Route path='*' element={<NotFoundPage />} />
       </Routes>
-      <Footer />
     </div>
   );
 }
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,18 +1,24 @@
 import React from 'react';
+import Header from '../Header/Header';
 import Promo from '../Main/Promo/Promo';
 import AboutProject from '../Main/AboutProject/AboutProject';
 import Techs from './Techs/Techs';
 import AboutMe from './AboutMe/AboutMe';
+import Footer from '../Footer/Footer';
 import './Main.css';
 
 function Main() {
   return (
-    <main className='content'>
-      <Promo />
-      <AboutProject />
-      <Techs />
-      <AboutMe />
-    </main>
+    <>
+      <Header />
+      <main className='content'>
+        <Promo />
+        <AboutProject />
+        <Techs />
+        <AboutMe />
+      </main>
+      <Footer />
+    </>
   );
 }
 
