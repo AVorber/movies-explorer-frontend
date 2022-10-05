de2db22c5f46b6c0d275886332ba0dedaf7a35af
a.rebrov <a.rebrov@cian.ru>
2022-10-06 01:24:21 +0300

    feat: добавляет бургер меню

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,32 +1,44 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import BurgerMenu from './BurgerMenu/BurgerMenu';
 import './Navigation.css';
 
 function Navigation({loggedIn}) {
+    const [isOpen, setIsOpen] = React.useState(false);
+
+  function handleOpenMenu() {
+    setIsOpen(true);
+  }
   return (
     <>
       { !loggedIn ? (
-        <div className='menu'>
-          <Link to='/sign-up' className='menu__link menu__link_weigt_m menu__link_color_white'  alt='Регистрация'>
+        <nav className='menu'>
+          <NavLink to='/sign-up' className='menu__link menu__link_weigt_m menu__link_color_white'  alt='Регистрация'>
             Регистрация
-          </Link>
-          <Link to='/sign-in' className='menu__link menu__link_button menu__link_weigt_m' alt='Войти'>
+          </NavLink>
+          <NavLink to='/sign-in' className='menu__link menu__link_button menu__link_weigt_m' alt='Войти'>
             Войти
-          </Link>
-        </div>
+          </NavLink>
+        </nav>
       ) : (
-        <div className='menu menu_logged-in'>
-          <Link to='/movies' className='menu__link menu__link_weigt_m menu__link_size_m' alt='Фильмы'>
-            Фильмы
-          </Link>
-          <Link to='/saved-movies' className='menu__link menu__link_size_m' alt='Сохранённые фильмы'>
-            Сохранённые фильмы
-          </Link>
-          <Link to='/profile' className='menu__profile-button' alt='Аккаунт'>
-            Аккаунт
-            <span className='menu__profile-icon' />
-          </Link>
-        </div>
+        <>
+          <nav className='menu menu_logged-in'>
+            <NavLink to='/movies' className='menu__link menu__link_weigt_m menu__link_size_m' alt='Фильмы'>
+              Фильмы
+            </NavLink>
+            <NavLink to='/saved-movies' className='menu__link menu__link_size_m' alt='Сохранённые фильмы'>
+              Сохранённые фильмы
+            </NavLink>
+            <NavLink to='/profile' className='menu__profile-button' alt='Аккаунт'>
+              Аккаунт
+              <span className='menu__profile-icon' />
+            </NavLink>
+          </nav>
+          <div className='menu__burger'>
+            <button className='menu__burger-button' aria-label='Бургер меню' type='button' onClick={handleOpenMenu} />
+            <BurgerMenu setIsOpen={setIsOpen} className={`burger-menu ${isOpen ? 'burger-menu_visible' : ''}`}/>
+          </div>
+        </>
       )}
     </>
   );
