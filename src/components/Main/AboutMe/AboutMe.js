601e49af7a89087ed6c1dfb0ad55636cf2aadc8d
a.rebrov <a.rebrov@cian.ru>
2022-10-05 20:03:18 +0300

    feat: добавляет фомфактор для основных страниц

--- src/components/Main/AboutMe/AboutMe.js
+++ src/components/Main/AboutMe/AboutMe.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import photo from '../../../images/foto.jpg';
+import photo from '../../../images/foto.svg';
 import Portfolio from '../Portfolio/Portfolio';
 import './AboutMe.css';
 
