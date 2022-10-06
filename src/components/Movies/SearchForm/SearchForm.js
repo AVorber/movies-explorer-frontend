e615082efa0daf1f768e037cfb780d1aea59dbbb
a.rebrov <a.rebrov@cian.ru>
2022-10-06 11:49:12 +0300

    feat: исправляет форму поиска

--- src/components/Movies/SearchForm/SearchForm.js
+++ src/components/Movies/SearchForm/SearchForm.js
@@ -1,21 +1,29 @@
 import React from 'react';
+import searchIcon from '../../../images/search-icon.svg';
 import './SearchForm.css';
 
 function SearchForm() {
   return (
     <section className='search'>
-      <form className='search__form'>
-        <input
-          className='search__form-input'
-          type='text'
-          placeholder='Фильм'
-        />
-        <button className='search__form-button' type='submit' />
-      </form>
-      <dev className='search__checkbox-container'>
-        <span className='search__checkbox-label'>Короткометражки</span>
-        <input className='search__checkbox' type='checkbox' />
-      </dev>
+      <div className='search__form-wrapper'>
+        <form className='search__form'>
+          <img
+            className='searchform__icon'
+            src={searchIcon}
+            alt='Поиск'
+          />
+          <input
+            className='search__form-input'
+            type='text'
+            placeholder='Фильм'
+          />
+          <button className='search__form-button' type='submit'>Найти</button>
+        </form>
+        <dev className='search__checkbox-container'>
+          <input className='search__checkbox' type='checkbox' />
+          <span className='search__checkbox-label'>Короткометражки</span>
+        </dev>
+      </div>
     </section>
   );
 }
